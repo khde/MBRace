0c91928056712e913e5cb4058d640790ece1fb7d
khde <118145809+khde@users.noreply.github.com>
2023-04-09 23:14:36 +0200

    Update mbrace.asm

--- mbrace.asm
+++ mbrace.asm
@@ -1,14 +1,11 @@
 ; mbrace.asm
+
     bits 16  ; real mode
     org 0x7c00  ; MBR in RAM
 
-
-; https://www.fountainware.com/EXPL/bios_key_codes.htm
 %define KEY_ESC 0x1
 %define KEY_UP 0x48
 %define KEY_DOWN 0x50
-%define KEY_LEFT 0x4b
-%define KEY_RIGHT 0x4d
 
 
 section .text
@@ -38,25 +35,65 @@ _up:
     dec word [ps]
 _down:
     cmp ah, KEY_DOWN
-    jne _left
+    jne _input_ret
     cmp [ps], word 0x3
-    je _left
+    je _input_ret
+
     add word [py], 6
     inc word [ps]
-_left:
-    cmp ah, KEY_LEFT
-    jne _right
-    dec word [px]
-_right:
-    cmp ah, KEY_RIGHT
-    jne _input_ret
-    inc word [px]
 _input_ret:
     ret
 
 
 logic:
+    ; new position for enemy 1
     dec word [e1x]
+    cmp word [e1x], 0
+    jnz _srs1
+    mov word [e1x], eoffset
+    
+    add [e1y], word 0x5
+    cmp [e1y], word 20
+    jnge _srs1
+    mov [e1y], word 0x3
+_srs1:
+
+    ; new position for enemy 2
+    dec word [e2x]
+    cmp word [e2x], 0x0
+    jnz _srs2
+    mov word [e2x], eoffset
+    
+    sub [e2y], word 0x5
+    cmp [e2y], word 0x0
+    jnle _srs2
+    mov [e2y], word 19
+_srs2:
+    
+    ; check collision
+    mov ax, [e1x]
+    mov bx, [e1y]
+    mov cx, 0x2
+_collp:
+    mov di, [px]  ; right
+    add di, 0xc
+    cmp di, ax
+    js _nocol
+    mov di, [py]  ; top
+    add di, 0x5
+    cmp di, bx
+    js _nocol
+    mov di, [py]  ; bottom
+    cmp bx, di
+    js _nocol
+    jmp start
+_nocol:
+    mov ax, [e2x]
+    mov bx, [e2y]
+    
+    dec cx
+    jnz _collp
+    
     ret
 
 
@@ -146,19 +183,14 @@ _clloop:
     inc di
     cmp di, ax
     jne _clloop
-    
-    ;xor  di, di
-    ;mov  cx, 80 * 25
-    ;mov  ax, 0x720      ; WhiteOnBlack space character
-    ;cld
-    ;rep stosw
+
     ret
 
 
 start:
     ; set video mode
     mov ah, 0x0
-    mov al, 0x2  ; 80x25? Text mode
+    mov al, 0x2  ; 80x25 text mode
     int 0x10
     
     ; set color of bg
@@ -175,18 +207,19 @@ start:
     mov ax, 0xb800  ; vga memory offset
     mov es, ax  ; move into segment register
     
-    ; player
+    ; player position
     mov word [ps], 0x1
     mov word [px], 0x6
     mov word [py], 0x3
     
-    ; enemy
+    ; enemy position
     mov word [e1x], 60 
-    mov word [e1y], 10
+    mov word [e1y], 6
     
-    mov word [e2x], 40
-    mov word [e2y], 18
-    
+    mov word [e2x], 25
+    mov word [e2y], 15
+
+
 game_loop:
     call input
     call logic
@@ -195,13 +228,14 @@ game_loop:
     ; wait
     xor ax, ax
     mov ah, 0x86  ; number for waiting
-    mov cx, 1  ; time to wait
+    mov cx, 0  ; time to wait
     int 0x15
     
     jmp game_loop
-    
+
+
 exit:
-    ; Linux kernel acpi/apm poweroff code
+    ; Linux kernel acpi or apm poweroff code
     ; blatantly copied
     mov ax, 0x1000
     mov ax, ss
@@ -212,26 +246,27 @@ exit:
     int 0x15
 
 
-;;;;;;;;;;;;;;;;;;
     ; player position
     ps dw 0x0
     px dw 0x0
     py dw 0x0
     
-    ; enemy
+    ; enemy 1
     e1x dw 0x0
     e1y dw 0x0
     
+    ; enemy2
     e2x dw 0x0
     e2y dw 0x0
     
-    eoffset dw 
+    ; x-respawn point for enemies
+    eoffset equ 65
     
     ; box
-    boxx dw 5
-    boxy dw 3
-    boxw dw 60
-    boxh dw 15
+    boxx db 5
+    boxy db 3
+    boxw db 60
+    boxh db 15
     
     ; car 12x5
 car:
